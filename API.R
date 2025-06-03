40a65d881b01b814e42847de629132daec4418bf
Iraia Laraudogoitia <iraia.laraudogoitia@alumni.mondragon.edu>
2025-06-03 12:56:42 +0200

    Api Echa

---
+++ API.R
@@ -0,0 +1,12 @@
+# API
+
+#* @apiTitle  API Examen Iraia Laraudogoitia
+#* @apiDescription  API que permite a los usuarios buscar areas que cumplan cierto criterio de calidad del agua
+#* @param CalidadAgua
+#* @get Water_q
+
+function(CalidadAgua){
+  CalidadAgua<- as.numeric(CalidadAgua)
+  filtrado<- datos%>% filter(Value >= CalidadAgua) %>% select (Area)
+  return(filtrado)
+}
